fe6f4ca3d8530aa6d95caa50bc97f64bc05fbce6
Aymane Mehdi <unlocked.dark.web@gmail.com>
2024-04-18 16:42:49 +0100

    f

--- src/components/sections/LatestPosts.jsx
+++ src/components/sections/LatestPosts.jsx
@@ -2,11 +2,38 @@ import Data from "@data/sections/latest-posts.json";
 import Date from "@library/date";
 import Link from "next/link";
 import ArrowIcon from "@layouts/svg-icons/Arrow";
-
+import Head from 'next/head';
+import Image from 'next/image';
 const LatestPostsSection = ({ posts, layout, imageHorizontal }) => {
+  
+  
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch('https://back-end-zlgx.onrender.com/blogs');
+      if (!response.ok) {
+        throw new Error('Failed to fetch posts');
+      }
+      const fetchedBlogs = await response.json();
+      setBlogs(fetchedBlogs);
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    }
+  
+  
+  
   return (
     <>
       {/* blog */}
+      <Head>
+  <title>FeizhouCom</title>
+  <meta name="description" content="FeizhouCom l'agence qui donne vie à votre identité,reflétant fidèlement votre essence et vos valeurs. " />
+  <meta name="keywords" content="seo, organisation, film, mangement, sites, anglais" />
+  <meta name="author" content="FeizhouCom" />
+    </Head>
       <section>
         <div className="container mil-p-120-60">
           <div className="row align-items-center mil-mb-30">
@@ -28,24 +55,25 @@ const LatestPostsSection = ({ posts, layout, imageHorizontal }) => {
             </div>
           </div>
           <div className="row">
-            {posts.slice(0, Data.numOfItems).map((item, key) => (
-              <div className="col-lg-6" key={`blog-post-${key}`}>
+          {blogs && blogs.slice(0, 2).map(blog => (
+            blog && (
+              <div className="col-lg-6" key={`blog-post-${blog._id}`}>
                 <Link
-                  href={`/blog/${item.id}`}
+                  href={`/blog/${blog._id}`}
                   className="mil-blog-card mil-mb-60"
                 >
                   <div className="mil-cover-frame mil-up">
-                    <img src={item.image} alt={item.title} />
+                  <Image src={blog.url} alt={blog.alt} width={500} height={300} />
                   </div>
                   <div className="mil-post-descr">
                     <div className="mil-labels mil-up mil-mb-30">
                       <div className="mil-label mil-upper">
-                        <Date dateString={item.date} />
+                        <Date dateString={blog.createdAt} />
                       </div>
                     </div>
-                    <h4 className="mil-up mil-mb-30">{item.title}</h4>
+                    <h4 className="mil-up mil-mb-30">{blog.title}</h4>
                     <p className="mil-post-text mil-up mil-mb-30">
-                      {item.short}
+                      {blog.text}
                     </p>
                     <div className="mil-link mil-dark mil-arrow-place mil-up">
                       <span>Lire plus</span>
@@ -54,7 +82,7 @@ const LatestPostsSection = ({ posts, layout, imageHorizontal }) => {
                   </div>
                 </Link>
               </div>
-            ))}
+            )))}
           </div>
         </div>
       </section>
